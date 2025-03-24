c7907a068ceb3fda8d8a047944c8d7ac8d6decd5
AndriusJur <58996191+AndriusJur@users.noreply.github.com>
2025-03-24 18:43:30 +0200

    Second  commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Deque<String> stack=new ArrayDeque<>();//not multithreaded
+
+        stack.push("first req");
+        stack.push("second req");
+        stack.push("third req");
+        System.out.println(stack);
+        System.out.println(stack.peek());
+        stack.pop();
+        System.out.println(stack);
     }
 }
